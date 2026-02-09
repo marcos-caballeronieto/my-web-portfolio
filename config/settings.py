c703e72d4f7dd09b29c4991a66c701c82bb50f5c
marcos-caballeronieto <marcos.caballeronieto@gmail.com>
2026-02-09 21:06:56 +0100

    fix: Changed secret key to .env for safety reasons

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vnsf658qy=+y3l+r64^(g@lm)-#5*017u-gpogc3mm+gzkp9fb'
+SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key-for-development')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
